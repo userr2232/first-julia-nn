c74b175e4bb7d6f69e4198f1ab7c7a100c67d70d
userr2232 <reynaldo.rz.26@gmail.com>
2022-01-31 14:23:43 -0500

    feat: script to run in parallel

--- job.sh
+++ job.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name HPO_CUDA
 #SBATCH --nodes=1
-#SBATCH --tasks-per-node=4
-#SBATCH --mem-per-cpu=1gb
+#SBATCH --tasks-per-node=10
+#SBATCH --mem-per-cpu=2gb
 #SBATCH --gres=gpu:1
 
 module load cuda
-python main.py root=$(pwd) action=run_study
+for i in {1..100}
+do
+    srun python main.py root=$(pwd) action=run_study &
+done
+wait
