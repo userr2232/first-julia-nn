ce26a57fb5a69229799fae79f882283a32da01b3
userr2232 <reynaldo.rz.26@gmail.com>
2022-02-01 12:06:48 -0500

    feat: Changed MariaDB to SQLite

--- local.sh
+++ local.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "$1" = -drop ]; then 
     echo Dropping database
-    mysql < db/drop.sql
+    # mysql < db/drop.sql
+    rm $(pwd)/db/first_julia_nn.db
 fi
-mysql < db/create.sql
 python main.py root=$(pwd) action=run_study training.device=cpu hpo.ntrials=1 hpo.max_nlayers=5 hpo.max_nunits=20 training.epochs=10
--- slurm.sh
+++ slurm.sh
@@ -6,11 +6,7 @@
 #SBATCH --gres=gpu:1
 
 module load cuda
-if [ "$1" = -drop ]; then 
-    echo Dropping database
-    mysql -u userr2232 < db/drop.sql
-fi
-mysql -u userr2232 < db/create.sql
+rm $(pwd)/db/first_julia_nn.db
 for i in {1..64}
 do
     srun python main.py root=$(pwd) action=run_study &
