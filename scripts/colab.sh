3bffc115d35da0c305a7eb6e6c6db79880fc46ad
userr2232 <reynaldo.rz.26@gmail.com>
2022-02-02 00:46:14 -0500

    fix: colab script

--- scripts/colab.sh
+++ scripts/colab.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-python main.py root=$(pwd)/first-julia-nn action=run_study
+if [ "$1" = -drop ]; then 
+    echo Dropping database
+    mysql -u userr2232 -h 127.0.0.1 < $(pwd)/first-julia-nn/db/drop.sql
+fi
+mysql -u userr2232 -h 127.0.0.1 < $(pwd)/first-julia-nn/db/create.sql
+python $(pwd)/first-julia-nn/main.py root=$(pwd)/first-julia-nn
+python $(pwd)/first-julia-nn/main.py root=$(pwd)/first-julia-nn action=run_study
