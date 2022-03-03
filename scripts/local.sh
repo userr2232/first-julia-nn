d86454c4189e0a6580d6d06b2042209ba129f227
userr2232 <reynaldo.rz.26@gmail.com>
2022-03-03 12:35:58 -0500

    fix: new local script

--- scripts/local.sh
+++ scripts/local.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-if [ "$1" = -drop ]; then 
-    echo Dropping database
-    mysql -u userr2232 -h 127.0.0.1 < db/drop.sql
-fi
-if [ -f $(pwd)/.env ]; then
+if [[ -f $(pwd)/.env ]]; then
     echo Exporting env vars
     export $(cat .env | xargs)
 fi
 ./credentials/cloud_sql_proxy -instances=deep-learning-308822:us-central1:hpo=tcp:3306 &
-sleep 2
+sleep 30
+if [[ "$1" = -drop ]]; then 
+    echo Dropping database
+    mysql -u userr2232 -h 127.0.0.1 < db/drop.sql
+fi
 mysql -u userr2232 -h 127.0.0.1 < db/create.sql
-python main.py root=$(pwd) action=run_study training.device=cpu hpo.ntrials=10
+for i in {1..4}; do
+    python main.py root=$(pwd) action=run_study training.device=cpu hpo.ntrials=200 &
+    if [[ $i = 1 ]]; then
+        sleep 10
+    fi
+done
+wait
+
