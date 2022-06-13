d622b8e5c1823c34862e5f770bc49b29bcb2dab8
userr2232 <reynaldo.rz.26@gmail.com>
2022-06-13 17:36:54 -0500

    fix: fixed colab.sh script

--- scripts/colab.sh
+++ scripts/colab.sh
@@ -4,5 +4,5 @@ if [ "$1" = -drop ]; then
     mysql -u userr2232 -h 127.0.0.1 < $(pwd)/first-julia-nn/db/drop.sql
 fi
 mysql -u userr2232 -h 127.0.0.1 < $(pwd)/first-julia-nn/db/create.sql
-python $(pwd)/first-julia-nn/main.py root=$(pwd)/first-julia-nn
+python $(pwd)/first-julia-nn/main.py root=$(pwd)/first-julia-nn action=create_partitions
 python $(pwd)/first-julia-nn/main.py root=$(pwd)/first-julia-nn action=run_study
