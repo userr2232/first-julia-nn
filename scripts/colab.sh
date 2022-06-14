eb3936107c06fc9671f88a248d3088c7713b0375
userr2232 <reynaldo.rz.26@gmail.com>
2022-06-14 12:55:02 -0500

    fix: fixed pyyaml conflict with plotly

--- scripts/colab.sh
+++ scripts/colab.sh
@@ -5,4 +5,4 @@ if [ "$1" = -drop ]; then
 fi
 mysql -u userr2232 -h 127.0.0.1 < $(pwd)/first-julia-nn/db/create.sql
 python $(pwd)/first-julia-nn/main.py root=$(pwd)/first-julia-nn action=create_partitions
-python $(pwd)/first-julia-nn/main.py root=$(pwd)/first-julia-nn action=run_study
+python $(pwd)/first-julia-nn/main.py root=$(pwd)/first-julia-nn action=run_study hpo.ntrials=50
